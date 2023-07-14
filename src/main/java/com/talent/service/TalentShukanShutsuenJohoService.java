bf7ca5e8aec65761f7016d5f744ce0267d5c0ae0
Fukuta-Y <yufukuta1008@gmail.co.jp>
2023-07-14 12:03:26 +0900

    mapperの追加

--- src/main/java/com/talent/service/TalentShukanShutsuenJohoService.java
+++ src/main/java/com/talent/service/TalentShukanShutsuenJohoService.java
@@ -1,8 +1,20 @@
 package com.talent.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.model.TalentShukanShutsuenJoho;
+import com.talent.infrastructure.dto.ChanelKyokuMasterDto;
+import com.talent.infrastructure.dto.KbnGenreMasterDto;
+import com.talent.infrastructure.dto.OnAirKanriTableDto;
+import com.talent.infrastructure.dto.ProgramMasterDto;
+import com.talent.infrastructure.dto.TalentMasterDto;
+import com.talent.infrastructure.repository.mapper.generated.MChanelKyokuMapper;
+import com.talent.infrastructure.repository.mapper.generated.MKbnGenreMapper;
+import com.talent.infrastructure.repository.mapper.generated.MProgramMapper;
+import com.talent.infrastructure.repository.mapper.generated.MTalentMapper;
+import com.talent.infrastructure.repository.mapper.generated.TOnairKanriMapper;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,6 +24,12 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class TalentShukanShutsuenJohoService {
+	
+    private final TOnairKanriMapper tOnairKanriMapper;
+    private final MTalentMapper mTalentMapper;
+    private final MProgramMapper mProgramMapper;
+    private final MChanelKyokuMapper mChanelKyokuMapper;
+    private final MKbnGenreMapper mKbnGenreMapper;
 
     /**
      * タレント週間出演情報検索
@@ -22,9 +40,14 @@ public class TalentShukanShutsuenJohoService {
      */
     public TalentShukanShutsuenJoho select(Integer nentsuki, Integer shu, String talentId) {
     	
-    	TalentShukanShutsuenJoho model = new TalentShukanShutsuenJoho();
-    	model.setmChanelKyoku(null);
-    	
-		return model;
+    	TalentShukanShutsuenJoho response = new TalentShukanShutsuenJoho();
+
+    	List<OnAirKanriTableDto> list1 = tOnairKanriMapper.select(nentsuki, shu, null);
+    	List<TalentMasterDto>  list2 = mTalentMapper.select(talentId);
+    	List<ProgramMasterDto> list3 = mProgramMapper.select(null);
+    	List<ChanelKyokuMasterDto> list4 = mChanelKyokuMapper.select(null);
+    	List<KbnGenreMasterDto> list5 = mKbnGenreMapper.select(talentId);
+
+		return response;
     }
 }
