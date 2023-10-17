0a60594f5ee9c51cc6ee359582fe7577e5ad9d69
Fukuta-Y <yufukuta1008@gmail.co.jp>
2023-10-17 11:43:22 +0900

    BE「番組登録・更新」の作成

--- generated/src/main/java/com/model/MProgram.java
+++ generated/src/main/java/com/model/MProgram.java
@@ -18,7 +18,7 @@ import jakarta.validation.constraints.Size;
 
 @Schema(name = "mProgram", description = "番組マスタ情報（全列）")
 @JsonTypeName("mProgram")
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-10-17T11:29:53.481023+09:00[Asia/Tokyo]")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-10-17T11:41:00.886316+09:00[Asia/Tokyo]")
 public class MProgram {
 
   @JsonProperty("programId")
